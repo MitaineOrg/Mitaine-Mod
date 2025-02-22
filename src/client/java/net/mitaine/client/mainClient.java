8fe895fc922f5608f66ab82e17f01939fe61f5b5
Corentin Arnaud <corentin.arnaud45@gmail.com>
2025-02-22 01:45:20 +0100

    First commit

---
+++ src/client/java/net/mitaine/client/mainClient.java
@@ -0,0 +1,10 @@
+package net.mitaine.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class mainClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
