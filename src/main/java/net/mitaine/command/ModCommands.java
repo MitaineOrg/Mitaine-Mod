ab4ef9430c79e708bf3c069b4fbaae49d1f74a3e
Corentin Arnaud <corentin.arnaud45@gmail.com>
2025-03-28 17:10:22 +0100

    Player custom name (but don't works)

--- src/main/java/net/mitaine/command/ModCommands.java
+++ src/main/java/net/mitaine/command/ModCommands.java
@@ -6,5 +6,6 @@ public class ModCommands {
 
     public static void registerCommands() {
         BraodcastCommand.register();
+        PlayerNameCommand.register();
     }
 }
--- src/main/java/net/mitaine/command/PlayerNameCommand.java
+++ src/main/java/net/mitaine/command/PlayerNameCommand.java
@@ -1,22 +1,34 @@
 package net.mitaine.command;
 
-import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.minecraft.command.argument.ColorArgumentType;
 import net.minecraft.server.command.CommandManager;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
+import java.util.Objects;
 
 public class PlayerNameCommand {
 
     public static void register() {
         CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> {
             dispatcher.register(CommandManager.literal("setPlayerRole")
-                    .then(CommandManager.argument("color", ColorArgumentType.color())
+                    .then(CommandManager.argument("role", StringArgumentType.string())
+                        .then(CommandManager.argument("color", StringArgumentType.string())
                             .executes(context -> {
-                                context.getSource().sendMessage(Text.literal("test"));
+                                String role = StringArgumentType.getString(context, "role");
+                                String color = StringArgumentType.getString(context, "color");
+                                try {
+                                    color = Formatting.valueOf(color.toUpperCase()).toString();
+                                    Objects.requireNonNull(context.getSource().getPlayer()).setCustomName(Text.literal(color + "[" + role + "] §r" + context.getSource().getPlayer().getName()));
+                                    Objects.requireNonNull(context.getSource().getPlayer()).setCustomNameVisible(true);
+                                    context.getSource().sendFeedback(() -> Text.literal("Player role set"), false);
+                                } catch (IllegalArgumentException e) {
+                                    context.getSource().sendMessage(Text.literal("Unknown color"));
+                                }
                                 return 1;
-                            })));
+                            }))));
         });
     }
 }
